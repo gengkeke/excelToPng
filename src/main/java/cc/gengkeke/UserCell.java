fe67992b666c1f4db624e6cf83c33fd466efe365
gengkeke <keke_geng@xinyan.com>
2019-04-22 13:58:44 +0800

    Initial commit

---
+++ src/main/java/cc/gengkeke/UserCell.java
@@ -0,0 +1,56 @@
+package cc.gengkeke;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Color;
+
+/**
+ * @Author: 18073004
+ * @Date: 2019/1/3 17:44
+ * @Description:
+ */
+public class UserCell {
+    private Cell cell;
+    private int row;
+    private int col;
+    private boolean show;
+    private String text="";
+    private Color color=null;
+
+    public Cell getCell() {
+        return cell;
+    }
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+    public int getRow() {
+        return row;
+    }
+    public void setRow(int row) {
+        this.row = row;
+    }
+    public int getCol() {
+        return col;
+    }
+    public void setCol(int col) {
+        this.col = col;
+    }
+    public boolean isShow() {
+        return show;
+    }
+    public void setShow(boolean show) {
+        this.show = show;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
